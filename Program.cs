cbcb24c986c3d9ad542489e53c4da835b813a33b
mustefa-dev <cs.21.065@student.uotechnology.edu.iq>
2025-05-31 22:11:20 +0300

    .

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 using VideoStreamApp.Hubs;
+using VideoStreamApp.Service;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", builder =>
-        builder.WithOrigins("http://localhost:3001", "https://localhost:3000")
+        builder.WithOrigins("http://localhost:8082", "https://localhost:3000")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials());
@@ -14,11 +16,28 @@ builder.Services.AddCors(options =>
 builder.Services.AddControllers();
 
 builder.Services.AddSignalR();
+builder.Services.AddSingleton<IRoomService, RoomService>();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "VideoStreamApp API", Version = "v1" });
+});
 
 var app = builder.Build();
 app.UseCors("CorsPolicy");
 app.MapControllers();
-app.MapHub<VideoHub>("/videosync");
+app.MapHub<VideoHub>("/videohub");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "VideoStreamApp API V1");
+        c.RoutePrefix = string.Empty; 
+    });
+}
 
 app.Run();
-
