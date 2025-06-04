7516aefe9f2a6f8afb02e45618b16d82fee35137
mustefa-dev <cs.21.065@student.uotechnology.edu.iq>
2025-06-04 08:14:39 +0300

    .

--- Program.cs
+++ Program.cs
@@ -2,14 +2,13 @@ using VideoStreamApp.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add this for detailed errors
 builder.Services.AddSignalR().AddNewtonsoftJsonProtocol();
 
 builder.Services.AddCors(options => 
 {
     options.AddPolicy("AllowVercel", policy => 
     {
-        policy.WithOrigins("https://farah-movie-cyan.vercel.app")
+        policy.WithOrigins("https://farah-movei.vercel.app")
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
